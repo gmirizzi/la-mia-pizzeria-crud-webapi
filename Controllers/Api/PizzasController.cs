0496e6b1fec6c9c3c6e141495165d886edaf1f1d
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-26 12:09:03 +0200

    metodo get api pizzas controller

--- Controllers/Api/PizzasController.cs
+++ Controllers/Api/PizzasController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers.Api
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PizzasController : ControllerBase
     {
+        [HttpGet]   
+        public IActionResult GetPizzas ()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                IQueryable<Pizza> pizze = db.Pizzas;
+                return Ok(pizze.ToList());
+            }
+        }
     }
 }
