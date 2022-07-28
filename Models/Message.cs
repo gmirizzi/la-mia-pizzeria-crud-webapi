ed70734dcc03765b2711f41550093b05c79c5d9c
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-28 14:21:59 +0200

    creato il model per i messaggi

---
+++ Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    [Table("messages")]
+    public class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+
+        [StringLength(25, ErrorMessage = "Il campo non può avere più di 25 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(25, ErrorMessage = "L'oggetto del messaggio non può avere più di 25 caratteri")]
+        public string Oggetto { get; set; }
+
+        [Required(ErrorMessage = "Il testo del messaggio non può essere vuoto")]
+        [Column(TypeName = "text")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
