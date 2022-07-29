93f174e300c4435490a6aad9b7ef1b8ba7b7fcee
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-29 12:28:57 +0200

    create method

---
+++ Models/Repositories/InMemoryPizzaRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace la_mia_pizzeria_static.Models.Repositories
+{
+    public class InMemoryPizzaRepository
+    {
+        private static List<Pizza> Pizzas = new List<Pizza>();
+
+        public void Create(Pizza pizza)
+        {
+            pizza.PizzaId = Pizzas.Count;
+            InMemoryPizzaRepository.Pizzas.Add(pizza);
+        }
+
+    }
+}
