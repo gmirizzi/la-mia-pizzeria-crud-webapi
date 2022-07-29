0caf20509a2a45ad5d36f0920db1af3de68702d0
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-29 12:47:07 +0200

    creata l'interfaccia per pizza repository

---
+++ Models/Repositories/Interfaces/IPizzaRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace la_mia_pizzeria_static.Models.Repositories.Interfaces
+{
+    public interface IPizzaRepository
+    {
+        public List<Pizza> GetList();
+        public Pizza GetById(int id);
+        public void Create(Pizza pizza);
+        public void Update(Pizza pizza);
+        public void Delete(Pizza pizza);
+    }
+}
